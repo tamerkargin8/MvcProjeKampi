1fc166f446e55f5e93a97658718896f8ef11c15a
Tamer Kargın <tamerkargin8@gmail.com>
2023-06-19 01:31:54 +0300

    40.Ders

--- BusinessLayer/Abstract/ICategoryService.cs
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -11,6 +11,9 @@ namespace BusinessLayer.Abstract
     {
         List<Category> GetCategoryList();
         void CategoryAdd(Category category);
+        Category GetByID(int id);
+        void CategoryDelete(Category category);
+        void CategoryUpdate(Category category);
     }
 
 }
--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,53 @@ namespace MvcProjeKampi.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryValues = cm.GetCategoryList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
             return View();
+        }
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryValue = cm.GetByID(id);
+            cm.CategoryDelete(categoryValue);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var categoryValue = cm.GetByID(id);
+            return View(categoryValue);
+        }        
+        [HttpPost]
+        public ActionResult UpdateCategory(Category c)
+        {
+            cm.CategoryUpdate(c);
+            return RedirectToAction("Index");
         }
     }
 }
